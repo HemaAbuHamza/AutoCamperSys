d3e0465f5b1bbe7b02496a5d0ca51970ac7db9b0
ayham <ayhamnth@hotmail.com>
2019-04-10 12:01:38 +0200

    update

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,4 +1,24 @@
 package UI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main  {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Auto Camper System");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        //just for test3
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
